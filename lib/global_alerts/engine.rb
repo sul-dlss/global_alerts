e9ffaa535a4417c1dad93c71d07fe2d3d7308807
Camille Villa <cvilla100@gmail.com>
2021-01-05 16:32:38 -0800

    Account for Rails 5 apps

--- lib/global_alerts/engine.rb
+++ lib/global_alerts/engine.rb
@@ -7,7 +7,8 @@ module GlobalAlerts
     config.url = 'https://github.com/sul-dlss/global-alerts/raw/main/sul.yaml'
 
     initializer('global_alerts_default') do |app|
-      config.application_name ||= app.class.module_parent_name.underscore
+      # parent_name is deprecated in Rails 6.1
+      config.application_name ||= app.class.respond_to?(:parent_name) ? app.class.parent_name.underscore : app.class.module_parent_name.underscore
     end
   end
 end
